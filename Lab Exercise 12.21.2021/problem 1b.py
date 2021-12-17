fcaa317291088f68d454c786d4f03c6702aa63ea
nmessa <nmessa@sau16.org>
2021-12-17 12:39:35 -0500

    Add files via upload

---
+++ Lab Exercise 12.21.2021/problem 1b.py
@@ -0,0 +1,11 @@
+## Lab Exercise 12/21/2021 Problem 1a
+## Author: nmessa
+## Draws rectangles using alternative graphics module
+
+from graphics import *
+
+def main():
+    #Add code here
+    
+
+main()
